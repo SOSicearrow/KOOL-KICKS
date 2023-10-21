92e59e5ce8eb2ce2abdec1ba772dfa14758b69e7
aumfreire <amf212@ensign.edu>
2023-10-21 13:43:11 -0600

    Finished Job

--- group4_Project1/js/script.js
+++ group4_Project1/js/script.js
@@ -1,32 +1,57 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function () {
+  let slideIndex = 0;
+  showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+  function plusSlides(n) {
+    slideIndex += n;
+    showSlides();
+  }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+  function currentSlide(n) {
+    slideIndex = n;
+    showSlides();
+  }
+//Start slideshow on slide number 1
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+
+  let prevButton = document.querySelector(".prev");
+  let nextButton = document.querySelector(".next");
+
+  prevButton.addEventListener("click", function () {
+    plusSlides(-1);
+  });
+
+  nextButton.addEventListener("click", function () {
+    plusSlides(1);
+  });
+
+  let dotButtons = document.getElementsByClassName("dot");
+  for (let i = 0; i < dotButtons.length; i++) {
+    dotButtons[i].addEventListener("click", function () {
+      currentSlide(i);
+    });
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+
+});
